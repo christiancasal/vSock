4861374a4c90eed56a49e43d99601f4484bbc24c
ccasal <casal.christian@gmail.com>
2017-01-16 19:44:07 -0500

    internal vs local storage

--- app/views/apartmentView/AddRoomModal.js
+++ app/views/apartmentView/AddRoomModal.js
@@ -31,7 +31,10 @@ export default class AddRoomModal extends Component {
   }
   componentWillMount(){
     //TODO: clear local storage
-    
+
+  }
+  clearLocalStorage(){
+
   }
   setContactModalVisible(visible){
     console.log('Close Modal');
